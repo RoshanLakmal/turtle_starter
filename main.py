3df5b76bf64a7e8b1b3ed131ea136c0c0ea3134d
Roshan <roshanrox911@gmail.com>
2023-08-29 21:30:42 +1000

    dash draw

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ turtle_start = Turtle()
 turtle_start.shape("turtle")
 turtle_start.color("red")
 
-for _ in range(4):
-    turtle_start.forward(100)
-    turtle_start.right(90)
+# Dash
+for _ in range(50):
+    turtle_start.forward(10)
+    turtle_start.penup()
+    turtle_start.forward(10)
+    turtle_start.pendown()
+
+# Squre
+# for _ in range(4):
+#     turtle_start.forward(100)
+#     turtle_start.right(90)
 
 # turtle_start.forward(100)
 # turtle_start.right(90)
@@ -14,6 +22,7 @@ for _ in range(4):
 # turtle_start.forward(100)
 # turtle_start.right(90)
 # turtle_start.forward(100)
+
 
 
 
